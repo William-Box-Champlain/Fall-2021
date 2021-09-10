d30695fcdd0e367b6f7aa1fc1bfdeca21751db9a
William-Box-Champlain <89941782+William-Box-Champlain@users.noreply.github.com>
2021-09-10 09:47:28 -0400

    Release Statement Update
    Added release statements to both projects to check for plagiarism.

--- Struct Project/main.cpp
+++ Struct Project/main.cpp
@@ -5,7 +5,28 @@
 //  Created by Brian R. Hall on 1/3/14.
 //  Copyright (c) 2014 Brian R. Hall. All rights reserved.
 //
+/*
 
+Author:       William Box
+Class : CSI - 240 - 01
+Assignment : Lab x
+Date Assigned : Sept - 03 - 21
+Due Date : Sept - 10 - 21@16:00
+
+Description :
+	uses a struct array to create an output a list of programmers with their ranks and average lines written
+
+	Certification of Authenticity :
+I certify that this is entirely my own work, except where I have given
+fully - documented references to the work of others.I understand the
+definition and consequences of plagiarism and acknowledge that the assessor
+of this assignment may, for the purpose of assessing this assignment :
+	-Reproduce this assignment and provide a copy to another member of academic staff;and /or
+	-Communicate a copy of this assignment to a plagiarism checking
+	service(which may then retain a copy of this assignment on its
+		database for the purpose of future plagiarism checking)
+
+*/
 #include <iostream>
 #include <string>
 #include <fstream>
