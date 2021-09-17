489ba6c111f0b244432f5f58c0a70c839f71335e
William-Box-Champlain <89941782+William-Box-Champlain@users.noreply.github.com>
2021-09-17 16:03:47 -0400

    Added Release Statement
    Added release statement, name, etc to edited files for projects.

--- Basic_Book_Class/book.cpp
+++ Basic_Book_Class/book.cpp
@@ -4,7 +4,22 @@
 #include <fstream>
 #include "book.h"
 #include <sstream>
-
+/*Author:William Box
+Class:CSI-240-01
+Assignment:Lab 3
+Date Assigned:Sept 6th
+Due Date:Sept 17th
+Description:Build a simple class about books
+Certification of Authenticity:
+I certify that this is entirely my own work, except where I have given
+fully-documented references to the work of others. I understand the
+definition and consequences of plagiarism and acknowledge that the assessor
+of this assignment may, for the purpose of assessing this assignment:
+- Reproduce this assignment and provide a copy to another member of academic staff; and/or
+- Communicate a copy of this assignment to a plagiarism checking
+service (which may then retain a copy of this assignment on its
+database for the purpose of future plagiarism checking)
+*/
 using namespace std;
 	//default constructor - not actually used but should be implemented anyway
 	Book::Book() 
--- Basic_Book_Class/main.cpp
+++ Basic_Book_Class/main.cpp
@@ -6,7 +6,22 @@
 //  Modified by: Murat K. Gungor 
 //  Thanks to Brian R. Hall
 //
-
+/*Author:William Box
+Class:CSI-240-01
+Assignment:Lab 3
+Date Assigned:Sept 6th
+Due Date:Sept 17th
+Description:Build a simple class about books
+Certification of Authenticity:    
+I certify that this is entirely my own work, except where I have given    
+fully-documented references to the work of others. I understand the    
+definition and consequences of plagiarism and acknowledge that the assessor    
+of this assignment may, for the purpose of assessing this assignment:        
+- Reproduce this assignment and provide a copy to another member of academic staff; and/or        
+- Communicate a copy of this assignment to a plagiarism checking           
+service (which may then retain a copy of this assignment on its          
+database for the purpose of future plagiarism checking)
+*/
 #include <iostream>
 #include <fstream>
 #include <string>
